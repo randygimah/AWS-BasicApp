bf39f5539e172524a6f135dcbca83f3bb29bc322
gyimah.randy <randy.gyimah@digital.mod.uk>
2025-10-04 19:26:55 +0100

    display user profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/*The code will use the Amplify Authenticator component to scaffold out 
+an entire user authentication flow allowing users to sign up, sign in, reset their password, 
+and confirm sign-in for multifactor authentication (MFA). */
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </StrictMode>
+);
